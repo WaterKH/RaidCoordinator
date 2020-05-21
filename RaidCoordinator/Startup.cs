f506b8b408fa4b137bb45b9d26ed925224d006bd
WaterKH <waterkh@outlook.com>
2020-05-21 16:47:49 -0400

    Moved logging

--- RaidCoordinator/Startup.cs
+++ RaidCoordinator/Startup.cs
@@ -26,10 +26,8 @@ namespace RaidCoordinator
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, ILogger<Startup> logger)
+        public void ConfigureServices(IServiceCollection services)
         {
-            logger.LogInformation(Configuration.GetConnectionString("RaidDatabase"));
-
             services.AddDbContext<RaidContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("RaidDatabase")), 
                 ServiceLifetime.Transient, ServiceLifetime.Transient);
@@ -49,8 +47,11 @@ namespace RaidCoordinator
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            logger.LogInformation(Configuration.GetConnectionString("RaidDatabase"));
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
